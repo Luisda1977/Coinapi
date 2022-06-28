d443a27291657b2837ac66663f43319810000231
Luisda <lsanchezroldan@gmail.com>
2022-06-28 21:23:23 +0200

    Buscamos las monedas que lleven "EUR"

--- llamada.py
+++ llamada.py
@@ -5,8 +5,8 @@ apikey = "92D803FB-637A-4A9E-83B5-15B457DF2C9B"
 cabeceras = {
     "X-CoinApi-Key": apikey
 }
-api_url = "http://rest-sandbox.coinapi.io"
-endpoint = "/v1/exchanges"
+api_url = "http://rest.coinapi.io"
+endpoint = "/v1/assets"
 
 url = api_url + endpoint
 
@@ -14,8 +14,13 @@ respuesta = requests.get(url, headers=cabeceras)
 codigo = respuesta.status_code
 
 if codigo == 200:
-    print("El resultado de la consulta es:")
-    print(respuesta.text)
+    print("Las monedas disponibles son:")
+    respuesta_json = respuesta.json()
+
+    for moneda in respuesta_json:
+        if moneda["asset_id"].startswith("EUR"):
+            print(moneda["asset_id"], moneda["name"])
+
 else:
     print("La petición a la API ha fallado")
     print(f"Código de error {codigo}")
