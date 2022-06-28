44bba7f562ff54a648229228e96a9825fbce2cf2
Luisda <lsanchezroldan@gmail.com>
2022-06-28 22:12:05 +0200

    Ya podemos realizar cambio.

--- exchange.py
+++ exchange.py
@@ -6,8 +6,25 @@ cabeceras = {
     "X-CoinApi-Key": apikey
 }
 
-moneda_origen = input("¿Qué moneda quieres cambiar?")
-moneda_destino = input("¿Qué moneda deseas obtener?")
 
-url = f"http://rest.coinapi.io/v1/exchangerate{moneda_origen}/{moneda_destino}"
-respuesta = requests.get(url, headers=cabeceras)
+seguir = "S"
+
+while seguir.upper() == "S":
+    moneda_origen = input("¿Qué moneda quieres cambiar?")
+    moneda_destino = input("¿Qué moneda deseas obtener?")
+
+    url = f"http://rest.coinapi.io/v1/exchangerate/{moneda_origen}/{moneda_destino}"
+    respuesta = requests.get(url, headers=cabeceras)
+    tipo_cambio = respuesta.json()
+
+    # Un euro son xxxxx bitcoins
+    # Un euro son xxxxx dólares
+    # Un bitcoin son xx dólares
+
+    cambio = tipo_cambio["rate"]
+    print("Un {} vale como {:,.2f} {}".format(
+        moneda_origen, cambio, moneda_destino))
+
+    seguir = ""
+    while seguir.upper() not in ("S", "N"):
+        seguir = input("¿Quieres hacer más cambio de monedas? (S/N) ")
