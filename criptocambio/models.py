a01a05f53172c9e21bef840e685644fc3fe672db
Luisda <lsanchezroldan@gmail.com>
2022-06-28 23:07:22 +0200

    Generamos una carpeta de criptocambio

---
+++ criptocambio/models.py
@@ -0,0 +1,45 @@
+import requests
+
+from . import APIKEY
+
+
+class APIError(Exception):
+    pass
+
+
+class CriptoModel:
+    """
+    -moneda_origen
+    -moneda_destino
+    -cambio
+    -consultar cambio (método)
+    """
+
+    def __init__(self, origen, destino):
+        """
+        Construyendo un obejto con las monedas origen y destino
+        y el cambio obtenido desde CoinApi inicializado a cero
+        """
+        self.moneda_origen = origen
+        self.moneda_destino = destino
+        self.cambio = 0.0
+
+    def consultar_cambio(self):
+        """
+        -Consulta el cambio entre la mondesa origen y la moneda destino
+        utlizando la API REST CoinApi
+        """
+        cabeceras = {
+            "X-CoinApi-Key": APIKEY
+        }
+        url = "http://rest.coinapi.io/v1/exchangerate/{moneda_origen}/{moneda_destino}"
+        respuesta = request.get(url, headers=cabeceras)
+
+        if respuesta.status_code == 200:
+            # guardo el cambio obtenido
+            self.cambio = respuesta.json()["rate"]
+        else:
+            raise APIError(
+                "Ha ocurrido un error {} {} al consultar la API." format(
+                    respuesta.status_code, respuesta.reason)
+            )
