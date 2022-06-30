eaa4fa213f047d5d7128a4e5adf00da267381e59
Luisda <lsanchezroldan@gmail.com>
2022-06-30 23:55:07 +0200

    Lo tenemos ok. Funciona nuestra aplicación.

--- criptocambio/__init__.py
+++ criptocambio/__init__.py
@@ -1,1 +1,8 @@
 APIKEY = "92D803FB-637A-4A9E-83B5-15B457DF2C9B"
+
+MONEDAS = ["EUR - Euro",
+           "USD - Dólar",
+           "BTC - Bitcoin",
+           "ETH - Ethereum",
+           "BNB - Binance coin",
+           "ADA - Cardano"]
--- criptocambio/controllers.py
+++ criptocambio/controllers.py
@@ -27,8 +27,20 @@ class CriptoController:
 class CriptoControllerTk(Tk):
     def __init__(self):
         super() .__init__()
-        self.vista = CriptoViewTk(self)
+        self.vista = CriptoViewTk(self, self.calcular_cambio)
         self.modelo = CriptoModel()
 
     def run(self):
         self.mainloop()
+
+    def calcular_cambio(self):
+        """
+        Recoge los datos de la vista
+        los pasa al modelo
+        pide el cambio al modelo
+        le pasa el resultado del cambio a la vista
+        """
+        self.modelo.moneda_origen = self.vista.moneda_origen()
+        self.modelo.moneda_destino = self.vista.moneda_destino()
+        self.modelo.consultar_cambio()
+        self.vista.mostrar_cambio(self.modelo.cambio)
